f77f36932f436649d9940c5429f0b479086adcc7
RealAnalysis <RealAnalysis@RealAnalysiss-iMac.local>
2020-08-04 20:03:47 +0800

    issue API token

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, :defaults => { :format => :json } do
@@ -19,4 +19,7 @@ Rails.application.routes.draw do
       post :update_temp
     end
   end
+
+  root "welcome#index"
+
 end
